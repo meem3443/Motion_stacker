f40446c7d7ac405e89fcb0ab86766ae0b4236706
전유정 <chun-yujeong@jeon-yujeong-ui-MacBookPro.local>
2025-02-11 17:23:24 +0900

    YJ_2_11.java 파일 추가

---
+++ CHUNYUJEONG/YJ_2_11.java
@@ -0,0 +1,26 @@
+package CHUNYUJEONG;
+//블로그 링크 : https://iamyujeong.tistory.com/20
+
+import java.util.*;
+
+public class YJ_2_11 {
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+
+        // 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
+        s.push(5);
+        s.push(2);
+        s.push(3);
+        s.push(7);
+        s.pop();
+        s.push(1);
+        s.push(4);
+        s.pop();
+        // 스택의 최상단 원소부터 출력
+        while (!s.empty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
